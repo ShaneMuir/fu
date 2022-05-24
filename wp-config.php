763fd5dc8b93f9996c2733780d7cb89fd12b11e9
Shane Muirhead <shane.muirhead@tribusdigital.com>
2022-05-24 11:33:35 +0100

    update

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+const WP_DEBUG = false;
+
+
 /**
  * Authentication Unique Keys and Salts.
  *
