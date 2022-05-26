9074a43ecf036dc28272c11657dac7560197c75b
Shane Muirhead <shane.muirhead@tribusdigital.com>
2022-05-26 13:15:17 +0100

    upgraded core to 6.0

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'fu' );
+define( 'DB_NAME', 'fictional_university' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
