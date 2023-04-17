7a009823ed2809a3710e39ef12c95bf82511fcb9
Shane Muirhead <shane.muirhead@tribusdigital.com>
2023-04-17 11:11:41 +0100

    removed editor feature for pages and removed comments since I don't use any of it

--- wp-content/themes/fu-theme/functions.php
+++ wp-content/themes/fu-theme/functions.php
@@ -168,3 +168,69 @@ function makeNotePrivate($data, $postarr) {
     return $data;
 }
 add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
+
+// Remove editor for pages since I don't ever use it
+function remove_editor_for_pages()
+{
+    remove_post_type_support('page', 'editor');
+}
+
+add_action('init', 'remove_editor_for_pages');
+
+// Completely Disable Comments and Trackbacks
+function fu_disable_comments_post_types_support() {
+    $post_types = get_post_types();
+    foreach ($post_types as $post_type) {
+        if(post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+}
+add_action('admin_init', 'fu_disable_comments_post_types_support');
+
+// Close comments on the front-end
+function fu_disable_comments_status(): bool
+{
+    return false;
+}
+add_filter('comments_open', 'fu_disable_comments_status', 20, 2);
+add_filter('pings_open', 'fu_disable_comments_status', 20, 2);
+
+// Hide existing comments
+function fu_disable_comments_hide_existing_comments($comments): array
+{
+    $comments = array();
+    return $comments;
+}
+add_filter('comments_array', 'fu_disable_comments_hide_existing_comments', 10, 2);
+
+// Remove comments page in menu
+function fu_disable_comments_admin_menu(): void
+{
+    remove_menu_page('edit-comments.php');
+}
+add_action('admin_menu', 'fu_disable_comments_admin_menu');
+
+// Redirect any user trying to access comments page
+function fu_disable_comments_admin_menu_redirect() {
+    global $pagenow;
+    if ($pagenow === 'edit-comments.php') {
+        wp_redirect(admin_url()); exit;
+    }
+}
+add_action('admin_init', 'fu_disable_comments_admin_menu_redirect');
+
+// Remove comments metabox from dashboard
+function fu_disable_comments_dashboard() {
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+}
+add_action('admin_init', 'fu_disable_comments_dashboard');
+
+// Remove comments links from admin bar
+function fu_disable_comments_admin_bar() {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+}
+add_action('init', 'fu_disable_comments_admin_bar');
